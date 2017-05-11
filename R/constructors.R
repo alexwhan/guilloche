9fdc289cef8ffba0f163d3a15a6680cd2b549d95
Alex Whan <alexwhan@gmail.com>
2017-05-12 00:25:36 +1000

    updated constructor to include pantograph point

--- R/constructors.R
+++ R/constructors.R
@@ -1,25 +1,35 @@
 #' Define a planetary orbit
 #'
 #' @param radius The radius of the orbit around its origin
-#' @param offset The position of the orbit center relative to the absolute origin
+#' @param offset The position of the orbit center relative to the parent orbit center
 #' @param speed The number of periods taken for a complete revolution
-#' @param parent_orbit
+#' @param pantograph_point The position of the pantograph attachment relative to the orbit center
+#' @param parent_orbit Name of an orbit object produced by `define_orbit()`
 #'
 #' @export
-define_orbit <- function(radius, offset, speed, parent_orbit = NULL) {
+define_orbit <- function(radius, offset, speed, pantograph_point = NULL, parent_orbit = NULL) {
   stopifnot(is.numeric(radius))
   stopifnot(is.numeric(offset))
   stopifnot(length(offset) == 2)
   stopifnot(is.numeric(speed))
   stopifnot(speed >= 0)
-  if(!is.null(parent_orbit)) stopifnot(class(parent_orbit) == "orbit")
+  if(!is.null(pantograph_point)) {
+    stopifnot(length(pantograph_point) == 2)
+    stopifnot(class(pantograph_point) == "numeric")
+  }
+  if(!is.null(parent_orbit)) {
+    if(class(parent_orbit) == "character") stop("The parent_orbit argument should be an unquoted object name")
+    parent_orbit_name <- deparse(substitute(parent_orbit))
+    if(!exists(parent_orbit_name)) message("Parent orbit object doesn't exist, this orbit will be defined without a parent")
+    else stopifnot(class(parent_orbit) == "orbit")
+  }
   orbit <- list(
     radius = radius,
     offset = offset,
     speed = speed
   )
 
-  if(!is.null(parent_orbit)) orbit$parent_orbit <- parent_orbit
+  if(!is.null(parent_orbit)) orbit$parent_orbit <- parent_orbit_name
   class(orbit) <- c("orbit")
   return(orbit)
 }
